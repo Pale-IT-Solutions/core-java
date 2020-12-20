db9c9b06aa1eb7a55c9f43cd172b0f51dab5a83c
kyiminhan <kyiminhan@localhost>
2020-12-20 19:11:56 +0900

    commit : update company size

--- 002-core-java/src/main/java/com/core/java/builder/CompanySize.java
+++ 002-core-java/src/main/java/com/core/java/builder/CompanySize.java
@@ -2,8 +2,16 @@ package com.core.java.builder;
 
 public enum CompanySize {
 
-	SIZE_25(25), SIZE_50(50), SIZE_100(100), SIZE_200(200), SIZE_400(400), SIZE_600(600), SIZE_800(800),
+	//@formatter:off
+	SIZE_25(25),
+	SIZE_50(50),
+	SIZE_100(100),
+	SIZE_200(200),
+	SIZE_400(400),
+	SIZE_600(600),
+	SIZE_800(800),
 	SIZE_1000(1000);
+	//@formatter:on
 
 	private int size;
 
@@ -21,5 +29,4 @@ public enum CompanySize {
 	public void setSize(final int size) {
 		this.size = size;
 	}
-
 }
